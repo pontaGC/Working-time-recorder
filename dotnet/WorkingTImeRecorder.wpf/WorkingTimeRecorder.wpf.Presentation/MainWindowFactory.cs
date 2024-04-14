bbd11ac130af5b3102ff0ad40ed0ce3c17a5df7e
pontaGC <makizawawa@gmail.com>
2024-04-14 16:20:59 +0900

    Sets main window view model.

--- dotnet/WorkingTImeRecorder.wpf/WorkingTimeRecorder.wpf.Presentation/MainWindowFactory.cs
+++ dotnet/WorkingTImeRecorder.wpf/WorkingTimeRecorder.wpf.Presentation/MainWindowFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using WorkingTimeRecorder.wpf.Presentation.Core;
 using WorkingTImeRecorder.wpf;
 
@@ -15,7 +10,11 @@ namespace WorkingTimeRecorder.wpf.Presentation
         /// <inheritdoc />
         public Window Create()
         {
-            return new MainWindow();
+            var viewModel = new MainWindowViewModel();
+            return new MainWindow()
+            {
+                DataContext = viewModel,
+            };
         }
     }
 }
