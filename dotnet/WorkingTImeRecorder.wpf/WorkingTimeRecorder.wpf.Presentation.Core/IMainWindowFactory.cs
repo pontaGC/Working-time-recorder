7f33fd2704375d4fddf64977e2b2c31033bacc5c
pontaGC <makizawawa@gmail.com>
2024-04-14 16:10:24 +0900

    Add WPF presentation projects.

---
+++ dotnet/WorkingTImeRecorder.wpf/WorkingTimeRecorder.wpf.Presentation.Core/IMainWindowFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace WorkingTimeRecorder.wpf.Presentation.Core
+{
+    /// <summary>
+    /// Responsible for creating a main window.
+    /// </summary>
+    public interface IMainWindowFactory
+    {
+        /// <summary>
+        /// Creates a main window.
+        /// </summary>
+        /// <returns>An instance of the main widnow.</returns>
+        Window Create();
+    }
+}
---
+++ dotnet/WorkingTImeRecorder.wpf/WorkingTimeRecorder.wpf.Presentation/MainWindowFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WorkingTimeRecorder.wpf.Presentation.Core;
+using WorkingTImeRecorder.wpf;
+
+namespace WorkingTimeRecorder.wpf.Presentation
+{
+    /// <inheritdoc />
+    internal sealed class MainWindowFactory : IMainWindowFactory
+    {
+        /// <inheritdoc />
+        public Window Create()
+        {
+            return new MainWindow();
+        }
+    }
+}
