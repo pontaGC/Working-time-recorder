a4cc005929af1184ec54239a824624db705b5320
pontaGC <makizawawa@gmail.com>
2024-05-06 04:17:20 +0900

    Add unhandled exception handlers.

--- dotnet/WorkingTImeRecorder.wpf/WorkingTImeRecorder.wpf/App.xaml.cs
+++ dotnet/WorkingTImeRecorder.wpf/WorkingTImeRecorder.wpf/App.xaml.cs
@@ -8,6 +8,7 @@ using WorkingTImeRecorder.Core.Injectors;
 using WorkingTimeRecorder.Core.Shared;
 using WorkingTimeRecorder.Core;
 using WorkingTimeRecorder.Core.Languages;
+using System.Windows.Threading;
 
 namespace WorkingTimeRecorder.wpf
 {
@@ -39,6 +40,8 @@ namespace WorkingTimeRecorder.wpf
         /// <inheritdoc />
         protected override void OnStartup(StartupEventArgs e)
         {
+            this.RegisterAppUnhandledExceptionHandler();
+
             var container = ContainerFactory.Create();
             RegisterDependencies(container, new WTRCoreDependencyRegistrant());
             RegisterDependencies(container, new PresentationDependencyRegistrant());
@@ -61,6 +64,70 @@ namespace WorkingTimeRecorder.wpf
             var appCulture = Config.GetCulture() ?? CultureNameConstants.DefaultCultureName; ;
             cultureSetter.SetCulture(appCulture);
         }
+
+        #region Unhandled exception handlers
+
+        private void RegisterAppUnhandledExceptionHandler()
+        {
+            // Unhandled exception caused in main thread (UI thread as WPF)
+            DispatcherUnhandledException += this.OnAppDispatcherUnhandledException;
+
+            // Unhandled exception caused in working threads
+            TaskScheduler.UnobservedTaskException += this.OnUnobservedTaskException;
+
+            AppDomain.CurrentDomain.UnhandledException += this.OnCurrentDomainUnhandledException;
+        }
+
+        private void OnAppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(
+                GetUnhandledErrorMessage(e.Exception),
+                WTRSystem.Instance.LanguageLocalizer.LocalizeAppTitle(),
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            e.Handled = true;
+
+            Environment.Exit(1);
+        }
+
+        private void OnUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            var exception = e.Exception.InnerException;
+            MessageBox.Show(
+                GetUnhandledErrorMessage(exception),
+                WTRSystem.Instance.LanguageLocalizer.LocalizeAppTitle(),
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            e.SetObserved();
+
+            Environment.Exit(1);
+        }
+
+        private void OnCurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = e.ExceptionObject as Exception;
+            MessageBox.Show(
+                GetUnhandledErrorMessage(exception),
+                WTRSystem.Instance.LanguageLocalizer.LocalizeAppTitle(),
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            Environment.Exit(1);
+        }
+
+        private static string GetUnhandledErrorMessage(Exception ex)
+        {
+            if (string.IsNullOrEmpty(ex?.Message))
+            {
+                return "The unexpected error has occurred.";
+            }
+
+            return $"The unexpected error has occurred.\r\n{ex.Message}";
+        }
+
+        #endregion
 
         #endregion
     }
---
+++ dotnet/WorkingTimeRecorder.Core/Extensions/LanguageLocalizerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+using WorkingTimeRecorder.Core.Languages;
+using WorkingTimeRecorder.Core.Shared;
+
+namespace WorkingTimeRecorder.Core.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="ILanguageLocalizer"/>.
+    /// </summary>
+    public static class LanguageLocalizerExtensions
+    {
+        /// <summary>
+        /// Gets the localized text of the title in this app.
+        /// </summary>
+        /// <param name="this">The instance.</param>
+        /// <returns>The localized text of the title in this app.</returns>
+        [DebuggerStepThrough]
+        public static string LocalizeAppTitle(this ILanguageLocalizer @this)
+        {
+            return @this.Localize(CommonTextKeys.Title, CommonTextKeys.DefaultTitle);
+        }
+    }
+}
