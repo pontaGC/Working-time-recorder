a700a6b3c484c4c2d0d149e06f012b84dd8bc75a
pontaGC <makizawawa@gmail.com>
2024-05-24 18:07:57 +0900

    Add Settings main menu items (only view).

--- dotnet/WorkingTimeRecorder.Core/Shared/Constants/WTRTextKeys.cs
+++ dotnet/WorkingTimeRecorder.Core/Shared/Constants/WTRTextKeys.cs
@@ -16,5 +16,13 @@
         public const string DefaultManHours = "Man-hours (h)";
         public const string RecordingTarget = "WorkingTimeRecorder.RecordingTarget";
         public const string DefaultRecordingTarget = "Recording Target";
+
+        // Menu items
+        public const string SettingsMenuItem = "WorkingTimeRecorder.MenuItem.Settings";
+        public const string DefaultSettingsMenuItem = "Settings";
+        public const string LanguageSettingMenuItem = "WorkingTimeRecorder.MenuItem.Settings.Languages";
+        public const string DefaultLanguageSettingMenuItem = "Languages";
+        public const string PersonDayMenuItem = "WorkingTimeRecorder.MenuItem.Settings.PersonDay";
+        public const string DefaultPersonDayMenuItem = "Man-hours per person day";
     }
 }
