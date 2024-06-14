13c8fd691d29e5612ebda072bdf3f9a1230174ab
pontaGC <makizawawa@gmail.com>
2024-06-15 06:51:24 +0900

    Add Undo/Redo icon.

--- dotnet/WorkingTimeRecorder.Core/Shared/Constants/IconKeys.cs
+++ dotnet/WorkingTimeRecorder.Core/Shared/Constants/IconKeys.cs
@@ -10,11 +10,15 @@
         [
             Add,
             Delete,
+            Undo, Redo,
             StatusError, StatusHelp, StatusInformation, StatusWarning,
         ];
 
         public const string Add = "Add";
         public const string Delete = "Delete";
+
+        public const string Undo = "Undo";
+        public const string Redo = "Redo";
 
         public const string StatusError = "StatusError";
         public const string StatusHelp = "StatusHelp";
