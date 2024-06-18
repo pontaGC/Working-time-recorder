b6ffcdf0421c62c257096ec02ed4eb557fe25e9d
pontaGC <makizawawa@gmail.com>
2024-06-19 08:07:14 +0900

    Add XmlSerializerHelper.

---
+++ dotnet/SharedLibraries/Extensions/StreamExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace SharedLibraries.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="Stream"/> object.
+    /// </summary>
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Checks whether or not the source stream is NULL or an empty stream.
+        /// </summary>
+        /// <param name="source">The source stream to check.</param>
+        /// <returns><c>false</c>, if the <c>source</c> stream is <c>null</c> or its <c>Length</c> is 0. Otherwise; <c>true</c>.</returns>
+        [DebuggerStepThrough]
+        public static bool IsNullOrEmpty(this Stream source)
+        {
+            return source is null || source.Length == 0;
+        }
+    }
+}
---
+++ dotnet/SharedLibraries/System/Xml/XmlSerializerHelper.cs
@@ -0,0 +1,72 @@
+﻿using System.Xml.Serialization;
+using SharedLibraries.Extensions;
+using SharedLibraries.Retry;
+
+namespace SharedLibraries.System.Xml
+{
+    /// <summary>
+    /// Responsible for serializing or deserializing the object related to XML document.
+    /// </summary>
+    public static class XmlSerializerHelper
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Try serializing the source object to a target stream.
+        /// </summary>
+        /// <typeparam name="T">The type of a source object.</typeparam>
+        /// <param name="source">The object to serialize. If <c>source</c> is <c>null</c>, this method does nothing.</param>
+        /// <param name="targetStream">The target stream to serialize.</param>
+        /// <param name="namespaces">The namespaces for then generated XML document.</param>
+        /// <returns><c>true</c>, if the serialization is success, Otherwise; <c>false</c>.</returns>
+        public static bool TrySerialize<T>(T source, Stream targetStream, XmlSerializerNamespaces namespaces = null)
+        {
+            if (source is null)
+            {
+                return false;
+            }
+
+            var serializer = new XmlSerializer(typeof(T));
+            try
+            {
+                RetryHelper.InvokeWithRetry(() => serializer.Serialize(targetStream, source, namespaces));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Deserializes the specified XML document to create the target object.
+        /// </summary>
+        /// <typeparam name="T">The type of a target object deserialized.</typeparam>
+        /// <param name="xmlStream">The XML document stream to deserialize.</param>
+        /// <param name="deserializedObject">The deserialized object which type is <c>T</c>.</param>
+        /// <returns><c>true</c>, if the deserialization is success, Otherwise; <c>false</c>.</returns>
+        public static bool TryDeserialize<T>(Stream xmlStream, out T deserializedObject)
+        {
+            deserializedObject = default;
+
+            if (xmlStream.IsNullOrEmpty())
+            {
+                return false;
+            }
+
+            var serializer = new XmlSerializer(typeof(T));
+            try
+            {
+                deserializedObject = (T)RetryHelper.InvokeWithRetry(() => serializer.Deserialize(xmlStream));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
